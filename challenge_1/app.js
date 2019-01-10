20c32cb13182cf18c38b75301452e857f0c7a542
PTSoup <phaimani.souphaphone@gmail.com>
2019-01-09 21:34:54 -0600

    Added click listener for cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,11 @@
 console.log(`reading app.js`)
+
+const square = document.getElementsByClassName('cell');
+
+function eventHandler(event) {
+    if (event.type === 'click') {
+        console.log('The cell was clicked!');
+    }
+};
+
+window.addEventListener('click', eventHandler);
