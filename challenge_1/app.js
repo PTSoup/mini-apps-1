4b9ce272b9e4f3db3417ee3703b903515b1c7ec3
PTSoup <phaimani.souphaphone@gmail.com>
2019-01-10 21:04:33 -0600

    Added X and O toggle on board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,13 +2,25 @@ console.log(`reading app.js`)
 
 const square = document.querySelector('table');
 
-function eventHandler(event) {
+//game elements
+var moveCount = 0;
+var player1 = [];
+var player2 = [];
+
+var current = 'X';
+var lastMove = null;
+
+//handles when a cell is clicked
+function clickHandler(event) {
     if (event.type === 'click') {
         console.log('The cell was clicked!');
         console.log(event.target.id);
+        document.getElementById(event.target.id).innerHTML=current;
+        moveCount++;
+        current = (current === 'X') ? 'O' : 'X';
     }
-
 };
+
 
 
 // for (var i = 0; i < square.length; i ++) {
@@ -17,4 +29,4 @@ function eventHandler(event) {
 // }  
 
 
-square.addEventListener('click', eventHandler);
+square.addEventListener('click', clickHandler);
