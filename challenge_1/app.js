8a7ed22cd56668936161905eac134f7c5cc2d835
PTSoup <phaimani.souphaphone@gmail.com>
2019-01-10 20:24:47 -0600

    Added click listener for board cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,20 @@
 console.log(`reading app.js`)
 
-const square = document.getElementsByClassName('cell');
+const square = document.querySelector('table');
 
 function eventHandler(event) {
     if (event.type === 'click') {
         console.log('The cell was clicked!');
+        console.log(event.target.id);
     }
+
 };
 
-window.addEventListener('click', eventHandler);
+
+// for (var i = 0; i < square.length; i ++) {
+//     var cellNumber = square[i].id;
+//     square[i].addEventListener('click', eventHandler);
+// }  
+
+
+square.addEventListener('click', eventHandler);
