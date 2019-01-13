eb65d838b70c7bedf1c6e2e0209b175217ff048d
PTSoup <phaimani.souphaphone@gmail.com>
2019-01-12 23:58:14 -0600

    App.post routing correctly
    App.post is routing. Does not produce the .CSV yet.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var app = express();
+
+app.listen(3000, 'localhost', () => {
+    console.log('App is listening');
+});
+
 
 app.use(express.static('client'));
 
+//posting json file
+app.post('/upload_json', (req, res) => {
+    res.send('You made it into the app');
+})
 
 module.exports = app;
