6eb89cd3ba34355d9623f2d4de306162f67c6958
PTSoup <phaimani.souphaphone@gmail.com>
2019-01-15 23:09:48 -0600

    App.js posting data to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,16 +1,28 @@
 var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.listen(3000, 'localhost', () => {
     console.log('App is listening');
 });
 
-
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('client'));
 
+
+app.get('/', (req, res) => {
+    res.send('/');
+})
+
 //posting json file
-app.post('/upload_json', (req, res) => {
-    res.send('You made it into the app');
+app.post('/upload', bodyParser.json(), (req, res) => {
+    var myBody = req.body;
+    console.log(myBody.Submit);
+    res.send(myBody);
 })
 
 module.exports = app;
